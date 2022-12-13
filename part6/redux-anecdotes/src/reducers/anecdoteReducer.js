d6c2687d6a87442183ac206dda634641718301c7
Didier van Os <ngon-@hotmail.com>
2022-12-13 13:30:26 +0000

    Complete part 6a

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,26 +4,51 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  console.log('state now: ', state);
+  console.log('action', action);
+  switch (action.type) {
+    case 'VOTE':
+      return state.map((a) =>
+        action.data.id === a.id ? { ...a, votes: a.votes + 1 } : a
+      );
+    case 'PUSH_ANECDOTE':
+      return state.concat(asObject(action.data.content));
+    default:
+      return state;
+  }
+  // return state
+};
 
-  return state
-}
+// Action creators
+// Take data, return action, consumer calls dispatch
+export const incrementVote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id },
+  };
+};
 
-export default reducer
+export const pushAnecdote = (content) => {
+  return {
+    type: 'PUSH_ANECDOTE',
+    data: { content },
+  };
+};
+
+export default reducer;
