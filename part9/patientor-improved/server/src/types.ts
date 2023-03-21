784adb979cdafb31b17f61b23ab6e2b7ed2864cb
Didi nGon- <ngon-@hotmail.com>
2023-03-21 10:08:32 +0000

    Implement zod serverside

--- part9/patientor-improved/server/src/types.ts
+++ part9/patientor-improved/server/src/types.ts
@@ -1,33 +1,23 @@
-// Define special omit for unions
+import { z } from 'zod';
+
+// Universal / utility
 export type UnionOmit<T, K extends string | number | symbol> = T extends unknown
   ? Omit<T, K>
   : never;
 
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
-}
+const isDate = (date: string) =>
+  /^\d{4}-\d{2}-\d{2}$/.test(date) && !Number.isNaN(Date.parse(date));
 
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other',
-}
-
-export enum EntryType {
-  HealthCheck = 'HealthCheck',
-  OccupationalHealthcare = 'OccupationalHealthcare',
-  Hospital = 'Hospital',
-}
-
-export interface BaseEntry {
-  id: string;
-  description: string;
-  date: string;
-  specialist: string;
-  diagnosisCodes?: Array<Diagnosis['code']>;
-}
+// Entry
+export const baseEntrySchema = z.object({
+  id: z.string(),
+  type: z.enum(['HealthCheck', 'OccupationalHealthcare', 'Hospital']),
+  description: z.string(),
+  date: z.string().refine(isDate, 'invalid date'),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+});
+// type BaseEntry = z.infer<typeof baseEntrySchema>;
 
 export enum HealthCheckRating {
   'Healthy' = 0,
@@ -35,54 +25,232 @@ export enum HealthCheckRating {
   'HighRisk' = 2,
   'CriticalRisk' = 3,
 }
+const healthCheckRatingEnum = z.nativeEnum(HealthCheckRating);
 
-export interface HealthCheckEntry extends BaseEntry {
-  type: 'HealthCheck';
-  healthCheckRating: HealthCheckRating;
-}
+const healthCheckEntrySchema = baseEntrySchema.extend({
+  type: z.literal('HealthCheck'),
+  healthCheckRating: healthCheckRatingEnum,
+});
+export type HealthCheckEntry = z.infer<typeof healthCheckEntrySchema>;
 
-// Define Entry without the 'id' property
-export type NewHealthCheckEntry = UnionOmit<HealthCheckEntry, 'id'>;
-
-export interface OccupationalHealthcareEntry extends BaseEntry {
-  type: 'OccupationalHealthcare';
-  employerName: string;
-  sickLeave?: {
-    startDate: string;
-    endDate: string;
-  };
-}
-export type NewOccupationalHealthcareEntry = UnionOmit<
-  OccupationalHealthcareEntry,
-  'id'
+const occupationalHealthcareEntrySchema = baseEntrySchema.extend({
+  type: z.literal('OccupationalHealthcare'),
+  employerName: z.string().min(3).max(64),
+  description: z.string(),
+  sickLeave: z
+    .object({
+      startDate: z.string(),
+      endDate: z.string(),
+    })
+    .optional(),
+});
+export type OccupationalHealthcareEntry = z.infer<
+  typeof occupationalHealthcareEntrySchema
 >;
 
-export interface HospitalEntry extends BaseEntry {
-  type: 'Hospital';
-  discharge: {
-    date: string;
-    criteria: string;
-  };
+const hospitalEntrySchema = baseEntrySchema.extend({
+  type: z.literal('Hospital'),
+  discharge: z.object({
+    date: z.string(),
+    criteria: z.string(),
+  }),
+});
+export type HospitalEntry = z.infer<typeof hospitalEntrySchema>;
+
+export const entrySchema = z.union([
+  hospitalEntrySchema,
+  occupationalHealthcareEntrySchema,
+  healthCheckEntrySchema,
+]);
+export type Entry = z.infer<typeof entrySchema>;
+export const newEntrySchema = z.union([
+  hospitalEntrySchema.omit({ id: true }),
+  occupationalHealthcareEntrySchema.omit({ id: true }),
+  healthCheckEntrySchema.omit({ id: true }),
+]);
+export type NewEntry = z.infer<typeof newEntrySchema>;
+export type EntryFormValues = UnionOmit<Entry, 'id'>;
+
+// Patient
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
+const genderSchema = z.nativeEnum(Gender);
+// type Gender = z.infer<typeof gender>
 
-export type NewHospitalEntry = UnionOmit<HospitalEntry, 'id'>;
+const patientSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  ssn: z.string(),
+  occupation: z.string(),
+  gender: genderSchema,
+  dateOfBirth: z.string(),
+  entries: z.array(entrySchema),
+});
+export type Patient = z.infer<typeof patientSchema>;
 
-export type Entry =
-  | HospitalEntry
-  | OccupationalHealthcareEntry
-  | HealthCheckEntry;
-
-export type NewEntry = UnionOmit<Entry, 'id'>;
-
-export interface Patient {
-  id: string;
-  name: string;
-  ssn: string;
-  occupation: string;
-  gender: Gender;
-  dateOfBirth: string;
-  entries: Entry[];
-}
-
+const patientFormValuesSchema = patientSchema.omit({ id: true, entries: true });
+export type PatientFormValues = z.infer<typeof patientFormValuesSchema>;
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
-export type NewPatient = Omit<Patient, 'id'>;
+export const newPatientSchema = patientSchema.omit({ id: true });
+export type NewPatient = z.infer<typeof newPatientSchema>;
+// export type NewPatient = Omit<Patient, 'id'>;
+
+// Diagnosis
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+// interface BaseEntry {
+//   id: string;
+//   description: string;
+//   date: string;
+//   specialist: string;
+//   diagnosisCodes?: Array<Diagnosis['code']>;
+// }
+
+// type HealthCheckRating = z.infer<typeof healthCheckRating>
+
+// export enum HealthCheckRating {
+//   'Healthy' = 0,
+//   'LowRisk' = 1,
+//   'HighRisk' = 2,
+//   'CriticalRisk' = 3,
+// }
+
+// export interface HealthCheckEntry extends BaseEntry {
+//   type: 'HealthCheck';
+//   healthCheckRating: HealthCheckRating;
+// }
+
+// export interface OccupationalHealthcareEntry extends BaseEntry {
+//   type: 'OccupationalHealthcare';
+//   employerName: string;
+//   description: string;
+//   sickLeave?: {
+//     startDate: string;
+//     endDate: string;
+//   };
+// }
+
+// export interface HospitalEntry extends BaseEntry {
+//   type: 'Hospital';
+//   discharge: {
+//     date: string;
+//     criteria: string;
+//   };
+// }
+
+// export type Entry =
+//   | HospitalEntry
+//   | OccupationalHealthcareEntry
+//   | HealthCheckEntry;
+
+// export type EntryFormValues = UnionOmit<Entry, 'id'>;
+
+// export interface Patient {
+//   id: string;
+//   name: string;
+//   ssn: string;
+//   occupation: string;
+//   gender: Gender;
+//   dateOfBirth: string;
+//   entries: Entry[];
+// }
+
+// export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+// export type NewPatient = Omit<Patient, 'id'>;
+// export type PatientFormValues = Omit<Patient, 'id' | 'entries'>;
+
+// // Define special omit for unions
+// export type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+//   ? Omit<T, K>
+//   : never;
+
+// export interface Diagnosis {
+//   code: string;
+//   name: string;
+//   latin?: string;
+// }
+
+// export enum Gender {
+//   Male = 'male',
+//   Female = 'female',
+//   Other = 'other',
+// }
+
+// export enum EntryType {
+//   HealthCheck = 'HealthCheck',
+//   OccupationalHealthcare = 'OccupationalHealthcare',
+//   Hospital = 'Hospital',
+// }
+
+// export interface BaseEntry {
+//   id: string;
+//   description: string;
+//   date: string;
+//   specialist: string;
+//   diagnosisCodes?: Array<Diagnosis['code']>;
+// }
+
+// export enum HealthCheckRating {
+//   'Healthy' = 0,
+//   'LowRisk' = 1,
+//   'HighRisk' = 2,
+//   'CriticalRisk' = 3,
+// }
+
+// export interface HealthCheckEntry extends BaseEntry {
+//   type: 'HealthCheck';
+//   healthCheckRating: HealthCheckRating;
+// }
+
+// // Define Entry without the 'id' property
+// export type NewHealthCheckEntry = UnionOmit<HealthCheckEntry, 'id'>;
+
+// export interface OccupationalHealthcareEntry extends BaseEntry {
+//   type: 'OccupationalHealthcare';
+//   employerName: string;
+//   sickLeave?: {
+//     startDate: string;
+//     endDate: string;
+//   };
+// }
+// export type NewOccupationalHealthcareEntry = UnionOmit<
+//   OccupationalHealthcareEntry,
+//   'id'
+// >;
+
+// export interface HospitalEntry extends BaseEntry {
+//   type: 'Hospital';
+//   discharge: {
+//     date: string;
+//     criteria: string;
+//   };
+// }
+
+// export type NewHospitalEntry = UnionOmit<HospitalEntry, 'id'>;
+
+// export type Entry =
+//   | HospitalEntry
+//   | OccupationalHealthcareEntry
+//   | HealthCheckEntry;
+
+// export type NewEntry = UnionOmit<Entry, 'id'>;
+
+// export interface Patient {
+//   id: string;
+//   name: string;
+//   ssn: string;
+//   occupation: string;
+//   gender: Gender;
+//   dateOfBirth: string;
+//   entries: Entry[];
+// }
+
+// export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+// export type NewPatient = Omit<Patient, 'id'>;
--- part9/patientor-improved/server/src/utils.ts
+++ part9/patientor-improved/server/src/utils.ts
@@ -1,217 +1,227 @@
 import {
-  NewPatient,
-  Gender,
-  NewHealthCheckEntry,
-  NewOccupationalHealthcareEntry,
-  NewHospitalEntry,
-  BaseEntry,
-  Diagnosis,
-  UnionOmit,
-  HealthCheckRating,
+  // NewPatient,
+  // Gender,
+  // NewHealthCheckEntry,
+  // NewOccupationalHealthcareEntry,
+  // NewHospitalEntry,
+  // BaseEntry,
+  // Diagnosis,
+  // UnionOmit,
+  // HealthCheckRating,
+  newEntrySchema,
+  newPatientSchema,
 } from './types';
 
 export const toNewEntry = (object: unknown) => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
-
-  if (
-    !(
-      'type' in object &&
-      'description' in object &&
-      'date' in object &&
-      'specialist' in object
-    )
-  ) {
-    throw new Error('Missing fields in medical entry');
-  }
-  if (typeof object.type !== 'string') {
-    throw new Error('Invalid medical entry type');
-  }
-
-  const baseEntry: UnionOmit<BaseEntry, 'id' | 'type'> = {
-    date: parseDate(object.date),
-    description: parseString(object.description),
-    specialist: parseString(object.specialist),
-  };
-
-  if ('diagnosisCodes' in object) {
-    if (!Array.isArray(object.diagnosisCodes)) {
-      throw new Error('Malformatted parameter diagnosisCodes');
-    }
-    baseEntry.diagnosisCodes = parseDiagnosisCodes(object);
-  }
-
-  switch (object.type) {
-    case 'HealthCheck': {
-      if (!('healthCheckRating' in object)) {
-        throw new Error('Something went wrong: missing healthCheckRating');
-      }
-      const entry: NewHealthCheckEntry = {
-        ...baseEntry,
-        type: object.type,
-        healthCheckRating: parseHealthCheckRating(object.healthCheckRating),
-      };
-      return entry;
-    }
-    case 'OccupationalHealthcare': {
-      if (!('employerName' in object)) {
-        throw new Error('Missing parameter');
-      }
-      const entry: NewOccupationalHealthcareEntry = {
-        ...baseEntry,
-        type: object.type,
-        employerName: parseString(object.employerName),
-      };
-      if ('sickLeave' in object) {
-        if (typeof object.sickLeave !== 'object' || object.sickLeave === null) {
-          throw new Error('invalid parameter sickLeave');
-        }
-        const { sickLeave } = object;
-        if (!('startDate' in sickLeave && 'endDate' in sickLeave)) {
-          throw new Error('missing parameters in sickLeave');
-        }
-        entry.sickLeave = {
-          startDate: parseDate(sickLeave.startDate),
-          endDate: parseDate(sickLeave.endDate),
-        };
-      }
-      return entry;
-    }
-    case 'Hospital': {
-      if (
-        !(
-          'discharge' in object &&
-          typeof object.discharge === 'object' &&
-          object.discharge !== null &&
-          'date' in object.discharge &&
-          'criteria' in object.discharge
-        )
-      ) {
-        throw new Error('Missing parameter');
-      }
-      const entry: NewHospitalEntry = {
-        ...baseEntry,
-        type: object.type,
-        discharge: {
-          criteria: parseString(object.discharge.criteria),
-          date: parseDate(object.discharge.date),
-        },
-      };
-      return entry;
-    }
-    default:
-      throw new Error('Invalid medical entry type');
-  }
+  return newEntrySchema.parse(object);
 };
 
-export const toNewPatient = (object: unknown): NewPatient => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
-  if (
-    'name' in object &&
-    'dateOfBirth' in object &&
-    'ssn' in object &&
-    'gender' in object &&
-    'occupation' in object
-  ) {
-    const newPatient: NewPatient = {
-      name: parseName(object.name),
-      dateOfBirth: parseDate(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation),
-      entries: [],
-    };
-
-    return newPatient;
-  }
-
-  throw new Error('Incorrect data: some fields are missing');
+export const toNewPatient = (object: unknown) => {
+  return newPatientSchema.parse(object);
 };
 
-// Type guard asserts type in compiler if TRUE is returned
-// Predicate (text is string) is asserted on that variable on subsequent calls
-const isString = (text: unknown): text is string =>
-  typeof text === 'string' || text instanceof String;
-
-const parseString = (text: unknown): string => {
-  if (!text || !isString(text)) {
-    throw new Error(`Expected string, received ${typeof text}`);
-  }
-  return text;
-};
-const isNumber = (num: unknown): num is number =>
-  typeof num === 'number' && !isNaN(num);
-
-// const parseNumber = (num: unknown): number => {
-//   if (!num || !isNumber(num)) {
-//     throw new Error(`Expected number, received ${typeof num}`);
+// export const toNewEntry = (object: unknown) => {
+//   if (!object || typeof object !== 'object') {
+//     throw new Error('Incorrect or missing data');
 //   }
-//   return num;
+
+//   if (
+//     !(
+//       'type' in object &&
+//       'description' in object &&
+//       'date' in object &&
+//       'specialist' in object
+//     )
+//   ) {
+//     throw new Error('Missing fields in medical entry');
+//   }
+//   if (typeof object.type !== 'string') {
+//     throw new Error('Invalid medical entry type');
+//   }
+
+//   const baseEntry: UnionOmit<BaseEntry, 'id' | 'type'> = {
+//     date: parseDate(object.date),
+//     description: parseString(object.description),
+//     specialist: parseString(object.specialist),
+//   };
+
+//   if ('diagnosisCodes' in object) {
+//     if (!Array.isArray(object.diagnosisCodes)) {
+//       throw new Error('Malformatted parameter diagnosisCodes');
+//     }
+//     baseEntry.diagnosisCodes = parseDiagnosisCodes(object);
+//   }
+
+//   switch (object.type) {
+//     case 'HealthCheck': {
+//       if (!('healthCheckRating' in object)) {
+//         throw new Error('Something went wrong: missing healthCheckRating');
+//       }
+//       const entry: NewHealthCheckEntry = {
+//         ...baseEntry,
+//         type: object.type,
+//         healthCheckRating: parseHealthCheckRating(object.healthCheckRating),
+//       };
+//       return entry;
+//     }
+//     case 'OccupationalHealthcare': {
+//       if (!('employerName' in object)) {
+//         throw new Error('Missing parameter');
+//       }
+//       const entry: NewOccupationalHealthcareEntry = {
+//         ...baseEntry,
+//         type: object.type,
+//         employerName: parseString(object.employerName),
+//       };
+//       if ('sickLeave' in object) {
+//         if (typeof object.sickLeave !== 'object' || object.sickLeave === null) {
+//           throw new Error('invalid parameter sickLeave');
+//         }
+//         const { sickLeave } = object;
+//         if (!('startDate' in sickLeave && 'endDate' in sickLeave)) {
+//           throw new Error('missing parameters in sickLeave');
+//         }
+//         entry.sickLeave = {
+//           startDate: parseDate(sickLeave.startDate),
+//           endDate: parseDate(sickLeave.endDate),
+//         };
+//       }
+//       return entry;
+//     }
+//     case 'Hospital': {
+//       if (
+//         !(
+//           'discharge' in object &&
+//           typeof object.discharge === 'object' &&
+//           object.discharge !== null &&
+//           'date' in object.discharge &&
+//           'criteria' in object.discharge
+//         )
+//       ) {
+//         throw new Error('Missing parameter');
+//       }
+//       const entry: NewHospitalEntry = {
+//         ...baseEntry,
+//         type: object.type,
+//         discharge: {
+//           criteria: parseString(object.discharge.criteria),
+//           date: parseDate(object.discharge.date),
+//         },
+//       };
+//       return entry;
+//     }
+//     default:
+//       throw new Error('Invalid medical entry type');
+//   }
 // };
 
-const parseDiagnosisCodes = (object: unknown): Array<Diagnosis['code']> => {
-  if (!object || typeof object !== 'object' || !('diagnosisCodes' in object)) {
-    // we will just trust the data to be in correct form
-    return [] as Array<Diagnosis['code']>;
-  }
+// export const toNewPatient = (object: unknown): NewPatient => {
+//   if (!object || typeof object !== 'object') {
+//     throw new Error('Incorrect or missing data');
+//   }
+//   if (
+//     'name' in object &&
+//     'dateOfBirth' in object &&
+//     'ssn' in object &&
+//     'gender' in object &&
+//     'occupation' in object
+//   ) {
+//     const newPatient: NewPatient = {
+//       name: parseName(object.name),
+//       dateOfBirth: parseDate(object.dateOfBirth),
+//       ssn: parseSsn(object.ssn),
+//       gender: parseGender(object.gender),
+//       occupation: parseOccupation(object.occupation),
+//       entries: [],
+//     };
 
-  return object.diagnosisCodes as Array<Diagnosis['code']>;
-};
+//     return newPatient;
+//   }
 
-const isDate = (date: string): boolean => Boolean(Date.parse(date));
-const parseDate = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-    throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
+//   throw new Error('Incorrect data: some fields are missing');
+// };
 
-console.log(Object.values(HealthCheckRating));
+// // Type guard asserts type in compiler if TRUE is returned
+// // Predicate (text is string) is asserted on that variable on subsequent calls
+// const isString = (text: unknown): text is string =>
+//   typeof text === 'string' || text instanceof String;
 
-const isHealthCheckRating = (param: number): param is HealthCheckRating =>
-  Object.values(HealthCheckRating).includes(param);
-const parseHealthCheckRating = (param: unknown): HealthCheckRating => {
-  if (!isNumber(param)) {
-    throw new Error(`healthCheckRating of invalid type: ${typeof param}`);
-  }
-  if (!isNumber(param) || !isHealthCheckRating(param)) {
-    throw Error(`Value of healthCheckRating incorrect: ${param}`);
-  }
-  return param;
-};
+// const parseString = (text: unknown): string => {
+//   if (!text || !isString(text)) {
+//     throw new Error(`Expected string, received ${typeof text}`);
+//   }
+//   return text;
+// };
+// const isNumber = (num: unknown): num is number =>
+//   typeof num === 'number' && !isNaN(num);
 
-const isGender = (gender: string): gender is Gender =>
-  Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(gender);
+// // const parseNumber = (num: unknown): number => {
+// //   if (!num || !isNumber(num)) {
+// //     throw new Error(`Expected number, received ${typeof num}`);
+// //   }
+// //   return num;
+// // };
 
-const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
-    throw new Error('Gender missing or of non-acceptable value' + gender);
-  }
-  return gender;
-};
+// const parseDiagnosisCodes = (object: unknown): Array<Diagnosis['code']> => {
+//   if (!object || typeof object !== 'object' || !('diagnosisCodes' in object)) {
+//     // we will just trust the data to be in correct form
+//     return [] as Array<Diagnosis['code']>;
+//   }
 
-const parseName = (name: unknown): string => {
-  if (!name || !isString(name)) {
-    throw new Error('Incorrect or missing name');
-  }
-  return name;
-};
+//   return object.diagnosisCodes as Array<Diagnosis['code']>;
+// };
 
-const parseSsn = (ssn: unknown): string => {
-  if (!ssn || !isString(ssn)) {
-    throw new Error('Incorrect or missing ssn');
-  }
-  return ssn;
-};
+// const isDate = (date: string): boolean => Boolean(Date.parse(date));
+// const parseDate = (date: unknown): string => {
+//   if (!date || !isString(date) || !isDate(date)) {
+//     throw new Error('Incorrect or missing date: ' + date);
+//   }
+//   return date;
+// };
 
-const parseOccupation = (occupation: unknown): string => {
-  if (!occupation || !isString(occupation)) {
-    throw new Error('Incorrect or missing occupation');
-  }
-  return occupation;
-};
+// console.log(Object.values(HealthCheckRating));
+
+// const isHealthCheckRating = (param: number): param is HealthCheckRating =>
+//   Object.values(HealthCheckRating).includes(param);
+// const parseHealthCheckRating = (param: unknown): HealthCheckRating => {
+//   if (!isNumber(param)) {
+//     throw new Error(`healthCheckRating of invalid type: ${typeof param}`);
+//   }
+//   if (!isNumber(param) || !isHealthCheckRating(param)) {
+//     throw Error(`Value of healthCheckRating incorrect: ${param}`);
+//   }
+//   return param;
+// };
+
+// const isGender = (gender: string): gender is Gender =>
+//   Object.values(Gender)
+//     .map((v) => v.toString())
+//     .includes(gender);
+
+// const parseGender = (gender: unknown): Gender => {
+//   if (!gender || !isString(gender) || !isGender(gender)) {
+//     throw new Error('Gender missing or of non-acceptable value' + gender);
+//   }
+//   return gender;
+// };
+
+// const parseName = (name: unknown): string => {
+//   if (!name || !isString(name)) {
+//     throw new Error('Incorrect or missing name');
+//   }
+//   return name;
+// };
+
+// const parseSsn = (ssn: unknown): string => {
+//   if (!ssn || !isString(ssn)) {
+//     throw new Error('Incorrect or missing ssn');
+//   }
+//   return ssn;
+// };
+
+// const parseOccupation = (occupation: unknown): string => {
+//   if (!occupation || !isString(occupation)) {
+//     throw new Error('Incorrect or missing occupation');
+//   }
+//   return occupation;
+// };
